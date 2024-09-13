98102392a5a78908b3749ab5a2532d897a1d4609
Joshua Nanninga <jmarknanninga@gmail.com>
2024-09-13 09:22:11 -0500

    added server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+const items = require('./routes/items');
+app.use('/api/items', items);
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
